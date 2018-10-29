7f5264d59e299b23329b5f30b2fb7a484e5a58e1
cxuereb <40347959+cxuereb@users.noreply.github.com>
2018-10-29 15:13:24 -0700

    Update bugs.php

--- bugs.php
+++ bugs.php
@@ -1,79 +1,68 @@
 <?php
-	// Connect to database
-	header("Content-Type: application/json; charset=UTF-8");
-	header('Access-Control-Allow-Origin: *');
+        // Connect to database
+        header("Content-Type: application/json; charset=UTF-8");
+        header('Access-Control-Allow-Origin: http://localhost:3000');
+        $host = 'dbserver.engr.scu.edu';
+        $user = 'eyale'; # enter your username
+        $password = 'tenspot10'; # enter your password
+        $dbname = 'sdb_' . $user;
 
-	$host = "dbserver.engr.scu.edu";
-	$user = "eyale"; # enter your username
-	$password = "tenspot10"; # enter your password
-	$dbname = "sdb_" . $user;
+        $conn = new mysqli($host, $user, $password, $dbname);
+        if ($conn->connect_error) {
+                die("Connection failed: $conn->connection_error");
+        }
+        $request_method = $_SERVER["REQUEST_METHOD"];
+        switch($request_method)
+                {
+                        case 'GET':
+                                get_bugs();
+                                //post_bugs();
+                                //$id = intval($_GET["id"]);
+                                //update_bugs($id);
+                                break;
+                        case 'POST':
+                                post_bugs();
+                                break;
+                        case 'UDPATE':
+                                $id = intval($_GET["id"]);
+                                update_bugs($id);
+                                break;
+                        default:
+                                // Invalid Request Method
+                                header("HTTP/1.0 405 Method Not Allowed");
+                                break;
+                }
 
-	$conn = new mysqli($host, $user, $password, $dbname);
-	if ($conn->connect_error) {
-		die("Connection failed: $conn->connection_error");
-	}
+        function get_bugs()
+        {
+                global $conn;
+                $query = "SELECT * FROM bugs_dev";
+                $response = array();
+                $result = mysqli_query($conn, $query);
+                while($row = mysqli_fetch_array($result))
+                {
+                        $response[] = $row;
+                }
+                header('Content-Type: application/json');
+                echo json_encode($response);
+        }
 
-	$request_method = $_SERVER["REQUEST_METHOD"];
-
-	switch($request_method)
-		{
-			case 'GET':
-				get_bugs();
-				break;
-
-			case 'POST':
-				post_bugs();
-				break;
-
-			case 'UDPATE':
-				$id = intval($_GET["id"]);
-				update_bugs($id);
-				break;
-			default:
-				// Invalid Request Method
-				header("HTTP/1.0 405 Method Not Allowed");
-				break;
-		}
-
-function get_bugs()
-	{
-		$query = "SELECT * FROM bugs_dev";
-		$response = array();
-		$result = mysqli_query($conn, $query);
-
-		while($row = mysqli_fetch_array($result))
-		{
-			$response[] = $row;
-		}
-
-		//header('Content-Type: application/json');
-		echo json_encode($response);
-
-	}
-
-function post_bugs()
- {
-		global $conn;
-
-		//$data = json_decode(file_get_contents('php://input'), true);
-		$bug_name = $data["bug_name"];
-
-		$name = $_POST["name"];
-		$type = $_POST["type"];
-		$severity = $_POST["severity"];
-		$description = $_POST["description"];
-		$program = $_POST["program"];
-		$browser = $_POST["browser"];
-
-		$query = "INSERT INTO bug_devs SET name='".$name."', type='".$type."',
-		severity='".$severity."', description='".$description."',program='".$program."',
-		browser='".$browser."',developer='".$assigned_developer."'";
-
-		if(mysqli_query($conn, $query))
-		{
-			$response = array(
-				'status' => 1,
-				'status_message' => 'Bug Added Successfully.'
+        function post_bugs()
+        {
+                global $conn;
+                $data = json_decode(file_get_contents('bug_form.html'), true);
+                $name = $data["name"];
+                $type = $data["type"];
+                $severity = $data["severity"];
+                $description = $data["description"];
+                $program = $data["program"];
+                $browser = $data["browser"];
+                $query = "INSERT INTO bug_devs (name, type, severity, description, program, browser) VALUES (".$name.",". $type.",". $severity.",". $description.",". $program.",". $browser.")";
+                if(mysqli_query($conn, $query))
+                {
+                        $response = array(
+                                'status' => 1,
+                                'status_message' => 'Bug Added Successfully.'
 			);
 		}
 
@@ -85,43 +74,36 @@ function post_bugs()
 			);
 		}
 
-		//header('Content-Type: application/json');
+		header('Content-Type: application/json');
 		echo json_encode($response);
 	}
 
 function update_bugs($id)
-	{
-		global $conn;
-		//$post_vars = json_decode(file_get_contents("php://input"),true);
-		$name = $_POST["name"];
-		$type = $_POST["type"];
-		$severity = $_POST["severity"];
-		$description = $_POST["description"];
-		$program = $_POST["program"];
-		$browser = $_POST["browser"];
-		$assigned_developer = $_POST["developer"];
+        {
+                global $conn;
+                $post_vars = json_decode(file_get_contents("bug_form.html"),true);
+                $assigned_developer = $post_vars["developer"];
+                $query = "UPDATE bug_devs SET developer = " . $assigned_developer . " WHERE id = " . $id;
 
-		$query = "UPDATE bug_devs SET name='".$name."', type='".$type."',
-		severity='".$severity."', description='".$description."',program='".$program."',
-		browser='".$browser."',developer='".$assigned_developer."' WHERE id=".$id;
+                if(mysqli_query($conn, $query))
+                {
+                        $response = array(
+                        'status' => 1,
+                        'status_message' =>'Bug Updated Successfully.'
+                        );
+                }
 
-		if(mysqli_query($conn, $query))
-		{
-			$response = array(
-			'status' => 1,
-			'status_message' =>'Bug Updated Successfully.'
-			);
-		}
+                else
+                {
+                        $response = array(
+                        'status' => 0,
+                        'status_message' =>'Bug Update Failed.'
+                        );
+                }
 
-		else
-		{
-			$response = array(
-			'status' => 0,
-			'status_message' =>'Bug Update Failed.'
-			);
-		}
+                header('Content-Type: application/json');
+                echo json_encode($response);
+        }
 
-		//header('Content-Type: application/json');
-		echo json_encode($response);
-	}
+        mysqli_close($conn);
 ?>
